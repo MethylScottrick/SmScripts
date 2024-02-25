0b48e9f987a34140e4a0ba2656091eb5942e7bb4
Scottrick <79002097+MethylScottrick@users.noreply.github.com>
2024-02-25 18:24:51 -0500

    1

---
+++ ChargeBar.lua
@@ -0,0 +1,27 @@
+﻿function drawChargeBar()
+  state = emu.getState()
+  sammov  = emu.read(0x0A1F,emu.memType.workRam)
+  if sammov ~= 1 then
+    return
+  end
+  anifrms = emu.readWord(0x0A94,emu.memType.workRam)
+  samani = emu.readWord(0x0A96,emu.memType.workRam)
+  spdcnt = emu.readWord(0x0B3F,emu.memType.workRam)
+  if spdcnt >= 4 then
+    emu.drawRectangle(58, 80, 140, 8, 0x3020FF20, true, 1)
+    emu.drawRectangle(58, 80, 140, 8, 0x30FFFFFF, false, 1)
+  else
+  anisleft = (9-samani)
+  timeleft = ((anisleft*3)+anifrms)
+  pctleft = 1-(timeleft/27)
+  if pctleft < 0 then
+    pctleft = 0
+  elseif pctleft > 1 then
+    pctleft = 1
+  end
+  barfill = 140*pctleft
+  emu.drawRectangle(58, 80, barfill, 8, 0x302060FF, true, 1)
+  emu.drawRectangle(58, 80, 140, 8, 0x30FFFFFF, false, 1)
+  end
+end
+emu.addEventCallback(drawChargeBar, emu.eventType.startFrame);
---
+++ TapMarkers.lua
@@ -0,0 +1,43 @@
+﻿wasMoving = false
+lineOffsets = {40,100,200}
+linePos = {}
+function drawTapMarkers()
+  state = emu.getState()
+  samusMoving  = emu.read(0x0A1F,emu.memType.workRam)
+  samusPosX = emu.readWord(0x0AF6,emu.memType.workRam)
+  samusPosXSub = emu.readWord(0x0AF8,emu.memType.workRam)
+  samusPosY = emu.readWord(0x0AFA,emu.memType.workRam)
+  samusPosYSub = emu.readWord(0x0AFC,emu.memType.workRam)
+  samusSpdX = emu.readWord(0x0B42,emu.memType.workRam)
+  samusSpdXSub = emu.readWord(0x0B44,emu.memType.workRam)
+  samusSpdY = emu.readWord(0x0B2E,emu.memType.workRam)
+  samusSpdYSub = emu.readWord(0x0B2C,emu.memType.workRam)
+  samusMomX = emu.readWord(0x0B46,emu.memType.workRam)
+  samusMomXSub = emu.readWord(0x0B48,emu.memType.workRam)
+  samusAnim = emu.readWord(0x0A96,emu.memType.workRam)
+  samusState = emu.readWord(0x0A1C,emu.memType.workRam)
+  samusDir = emu.read(0x0A1E,emu.memType.workRam)
+  samusMoveType = emu.read(0x0A1F,emu.memType.workRam)
+  samusSpeedLevel = emu.readWord(0x0B3F,emu.memType.workRam)
+  screenX = emu.readWord(0x0A96,emu.memType.workRam)
+  screenY = emu.readWord(0x0A96,emu.memType.workRam)
+  framesToAnim = emu.readWord(0x0A94,emu.memType.workRam)
+  if samusMoving == 1 then
+    if not wasMoving then
+      --calc line pos's
+      facingMult = {}
+      if samusDir == 4 then
+        facingMult = -1
+      else
+        facingMult = 1
+      end
+      --TODO
+    end
+    --draw lines
+    
+    wasMoving = true
+  else
+    wasMoving = false
+  end
+end
+emu.addEventCallback(drawTapMarkers, emu.eventType.startFrame);
